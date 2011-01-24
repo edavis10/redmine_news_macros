25429830114872432b8f53af9be926613bfaca54
Eric Davis <edavis@littlestreamsoftware.com>
2011-01-24 14:47:43 -0800

    [#4285] Updated init.rb

--- init.rb
+++ init.rb
@@ -1,10 +1,11 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_news_macros do
-  name 'Redmine News Macros plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  name 'News Macros'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-misc'
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'This plugin adds wiki macros for News items'
+  version '0.1.0'
 end
+
